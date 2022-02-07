60d5eed1d6a933219a435403f1db24a5a684cab2
Mateus <mateusmelosf@gmail.com>
2022-02-07 14:51:03 -0300

    aula 1 completa

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response, NextFunction} from "express";
+import statusRoute from "./routes/status.route";
 import usersRoute from "./routes/users.route";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(statusRoute);
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
